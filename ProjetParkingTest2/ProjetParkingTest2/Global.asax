8010a03b807d82190604477a4c9c29fa6018865d
StephenAloche <stephen.aloche@gmail.com>
2018-05-30 10:05:25 +0200

    initCommit

---
+++ ProjetParkingTest2/ProjetParkingTest2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProjetParkingTest2.MvcApplication" Language="C#" %>
---
+++ ProjetParkingTest2/ProjetParkingTest2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjetParkingTest2.Startup))]
+namespace ProjetParkingTest2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
