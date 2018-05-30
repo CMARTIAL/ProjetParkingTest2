ffce73b059493f93f4e8bcf09718647af1fbd6e2
CMARTIAL <sio.martial.corentin@gmail.com>
2018-05-30 11:26:12 +0200

    Initial BO

---
+++ ProjetParkingTest2/BO/Evenement.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO
+{
+    public class Evenement : IEntityIdentifiable
+    {
+        public Evenement(Guid id, string titre, Adresse adresseEvenement, int duree, string theme, int tarif, string description, ICollection<Image> images, DateTime dateEvenement)
+        {
+            Id = id;
+            Titre = titre;
+            AdresseEvenement = adresseEvenement;
+            Duree = duree;
+            Theme = theme;
+            Tarif = tarif;
+            Description = description;
+            Images = images;
+            DateEvenement = dateEvenement;
+        }
+
+        public Evenement() { }
+
+        public Guid Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Titre { get; set; }
+        public Adresse AdresseEvenement { get; set; }
+        public int Duree { get; set; }
+        public string Theme { get; set; }
+        public int Tarif { get; set; }
+        public string Description { get; set; }
+        public virtual ICollection<Image> Images { get; set; }
+        public DateTime DateEvenement { get; set; }
+
+    }
+}
---
+++ ProjetParkingTest2/BO/Parking.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO
+{
+    public class Parking : IEntityIdentifiable
+    {
+        public Parking(Guid id, string titre, Adresse adressePark, int nBPlaceTotal, int nBPlaceLibre, int tarif, TimeSpan heureOuverture, TimeSpan heureFermeture, string statut)
+        {
+            Id = id;
+            Titre = titre;
+            AdressePark = adressePark;
+            NBPlaceTotal = nBPlaceTotal;
+            NBPlaceLibre = nBPlaceLibre;
+            Tarif = tarif;
+            HeureOuverture = heureOuverture;
+            HeureFermeture = heureFermeture;
+            Statut = statut;
+        }
+
+        public Parking() { }
+
+        public Guid Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Titre { get; set; }
+        public Adresse AdressePark { get; set; }
+        public int NBPlaceTotal { get; set; }
+        public int NBPlaceLibre { get; set; }
+        public int Tarif { get; set; }
+        public TimeSpan HeureOuverture { get; set; }
+        public TimeSpan HeureFermeture { get; set; }
+        public string Statut { get; set; }
+
+    }
+}
