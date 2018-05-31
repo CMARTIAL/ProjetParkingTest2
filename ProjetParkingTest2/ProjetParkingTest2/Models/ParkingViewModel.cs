cf181b8c7afd87a1202b34ec875f92f14bdd95b2
StephenAloche <stephen.aloche@gmail.com>
2018-05-31 12:07:36 +0200

    ajout parking en base + modif parkingviewmodel

--- ProjetParkingTest2/ProjetParkingTest2/Models/ParkingViewModel.cs
+++ ProjetParkingTest2/ProjetParkingTest2/Models/ParkingViewModel.cs
@@ -1,9 +1,12 @@
 ﻿using BO;
 using DAL;
+using Newtonsoft.Json;
 using Services;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Net;
 using System.Web;
 
 namespace ProjetParkingTest2.Models
@@ -17,6 +20,78 @@ namespace ProjetParkingTest2.Models
         public ParkingViewModel(Parking parking)
         {
             this.Metier = parking;
+        }
+
+        [Display(Name = "Nom du Parking")]
+        public string Titre {
+            get { return Metier.Titre; }
+            set { Metier.Titre = value; }
+        }
+        [Display(Name = "Nombre de places totals")]
+        public int NBPlaceTotal {
+            get { return Metier.NBPlaceTotal; }
+            set { Metier.NBPlaceTotal = value; }
+        }
+        [Display(Name = "Nombre de places libres")]
+        public int NBPlaceLibre {
+            get { return Metier.NBPlaceLibre; }
+            set { Metier.NBPlaceLibre = value; }
+        }
+        [Display(Name = "Statut du parling")]
+        public string Statut {
+            get { return Metier.Statut; }
+            set { Metier.Statut = value; }
+        }
+
+        [Display(Name = "Latittude ?")]
+        public double Coordonee0 {
+            get { return Metier.Coordonee0; }
+            set { Metier.Coordonee0 = value; }
+        }
+        [Display(Name = "Longitude ?")]
+        public double Coordonee1
+        {
+            get { return Metier.Coordonee1; }
+            set { Metier.Coordonee1 = value; }
+        }
+
+
+
+        public static void AddtoBase()
+        {
+            List<Parking> parkingToAdd = new List<Parking>();
+            //recupere les parkings
+            using (ParkingContext context = new ParkingContext())
+            {
+                using (WebClient wc = new WebClient())
+                {
+                    var json = wc.DownloadString("http://data.citedia.com/r1/parks/");
+                    dynamic data = JsonConvert.DeserializeObject<RootObject>(json);
+                    foreach (Park park in data.parks)
+                    {
+                        Parking parking = new Parking(Guid.NewGuid(),
+                                                      park.parkInformation.name,
+                                                      null,
+                                                      park.parkInformation.max,
+                                                      park.parkInformation.free,
+                                                      0, new TimeSpan(0, 0, 0),
+                                                      new TimeSpan(0, 0, 0),
+                                                      park.parkInformation.status
+                                                      );
+                        foreach (Feature feature in data.features.features)
+                        {
+                            if (feature.id == park.id)
+                            {
+                                parking.Coordonee0 = feature.geometry.coordinates[0];
+                                parking.Coordonee1 = feature.geometry.coordinates[1];
+                            }
+                        }
+                        parkingToAdd.Add(parking);
+                    }
+                }
+                context.Parkings.AddRange(parkingToAdd);
+                context.SaveChanges();
+            }
         }
 
         /// <summary>
