1c953e8efa00ba4ccc2a27641be743b116c14376
StephenAloche <stephen.aloche@gmail.com>
2018-06-01 11:02:52 +0200

    googlemap

--- ProjetParkingTest2/ProjetParkingTest2/Controllers/HomeController.cs
+++ ProjetParkingTest2/ProjetParkingTest2/Controllers/HomeController.cs
@@ -10,6 +10,8 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Script.Serialization;
+using System.Web.UI.WebControls;
+using System.Xml;
 
 namespace ProjetParkingTest2.Controllers
 {
@@ -39,5 +41,30 @@ namespace ProjetParkingTest2.Controllers
         { 
             return View();
         }
+
+        public ActionResult GoogleMaps()
+        {
+            return View();
+        }
+
+        // Edit? adresseConvive = sdsdsds & Id = da5f6d63 - 40e9 - 4fdd-9ac8-621edb427ecd
+        // GET: Evenement/Edit/5
+        public ActionResult Edit(String adresseConvive, Guid Id)
+        {
+            EvenementViewModel eVM = EvenementViewModel.GetByGuid(Id);
+            adresseConvive = adresseConvive.Replace(" ", "+");
+            string query = "https://nominatim.openstreetmap.org/search?q=" + adresseConvive;
+            
+            string xmlStr;
+            using (var wc = new WebClient())
+            {
+                xmlStr = wc.DownloadString(query);
+            }
+            var xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(xmlStr);
+
+            return View();
+        }
+
     }
 }
